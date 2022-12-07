f25f902ad1d2771efa640e945808daaf13d49c51
Jennie Allen <jenanemone@gmail.com>
2022-12-06 18:02:22 -0600

    LeetCode Easy 771 Jewels and Stones

---
+++ leetcode_solutions/771-jewelsAndStones.js
@@ -0,0 +1,48 @@
+/* PROBLEM ::: LeetCode Easy 771. Jewels and Stones
+
+https://leetcode.com/problems/jewels-and-stones/description/
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+ 
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+*/
+
+// SOLUTION :::
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+ var numJewelsInStones = function(jewels, stones) {
+    let jewelObj = {};
+    jewels.split("").forEach((stone) => {
+        jewelObj[stone] ? jewelObj[stone]++ : jewelObj[stone] = 1;
+    });
+    let numJewels = 0;
+    stones.split("").forEach((stone) => {
+        if (stone in jewelObj) {
+            numJewels++;
+        }
+    });
+
+    return numJewels;
+};
